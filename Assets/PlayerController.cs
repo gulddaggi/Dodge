51d0e756680eb3accefd75bd0747ec463857bf32
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2023-02-08 16:47:29 +0900

    PlayerController edit

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,32 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Rigidbody playerRigidbody;
+    private Rigidbody playerRigidbody;
     public float speed = 8f;
 
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.UpArrow) == true)   
-        {
-            playerRigidbody.AddForce(0f, 0f, speed);
-        }
-        if (Input.GetKey(KeyCode.DownArrow) == true)
-        {
-            playerRigidbody.AddForce(0f, 0f, -speed);
-        }
-        if (Input.GetKey(KeyCode.RightArrow) == true)
-        {
-            playerRigidbody.AddForce(speed, 0f, 0f);
-        }
-        if (Input.GetKey(KeyCode.LeftArrow) == true)
-        {
-            playerRigidbody.AddForce(-speed, 0f, 0f);
-        }
+
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        float xSpeed = xInput * speed;
+        float zSpeed = zInput * speed;
+
+        Vector3 newvelocity = new Vector3(xSpeed, 0, zSpeed);
+
+        playerRigidbody.velocity = newvelocity;
     }
 
     public void Die()
